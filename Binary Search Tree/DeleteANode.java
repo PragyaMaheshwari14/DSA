c100ef2d853433fd18df2cec3310b22100ce2baf
Pragya Maheshwari <pragyam1403@gmail.com>
2023-09-09 23:01:41 +0530

    01

--- Binary Search Tree/DeleteANode.java
+++ Binary Search Tree/DeleteANode.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class DeleteANode {
     static class Node {
         int data;
@@ -70,6 +72,46 @@ public class DeleteANode {
         return root;
     }
 
+    public static void printInRange(Node root , int k1 , int k2) {
+        if(root == null ) {
+            return;
+        }
+        if(root.data >= k1 && root.data<=k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data +" ");
+            printInRange(root.right, k1, k2);
+        }
+        else if (root.data <= k1) {
+            printInRange(root.left, k1, k2);
+        }
+        else {
+            printInRange(root.right, k1, k2);
+        }
+    }
+     
+    public static void printPath(ArrayList<Integer> path) {
+        for(int i=0; i<path.size( ); i++) {
+            System.out.print(path.get(i) + " ->");
+        }
+        System.out.println("Null");
+    }
+
+    public static void printRoot2Leaf(Node root , ArrayList<Integer> path) {
+        if(root == null) {
+            return;
+        }
+
+       path.add(root.data);
+       if(root.left == null && root.right == null) {
+            printPath(path);
+        }
+
+       printRoot2Leaf(root.left, path);
+       printRoot2Leaf(root.right, path);
+       path.remove(path.size()-1);
+    }
+ 
+
     public static void main(String args[]) {
         int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
@@ -81,10 +123,13 @@ public class DeleteANode {
        inorder(root);
         System.out.println();
 
-        root = delete(root, 1);
-        System.out.println();
-    
-        inorder(root);
+        // root = delete(root, 1);
+        // System.out.println();
+       // inorder(root);
+
+      // printInRange(root, 5, 12);
+
+      printRoot2Leaf(root, new ArrayList<>());
 
     }
 }
